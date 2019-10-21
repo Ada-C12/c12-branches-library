2c737ba4444bc6d1b3fb76bc2ea6e19d764c63e7
Jared Maddox <jared@adadevelopersacademy.org>
2019-10-21 12:15:21 -0700

    Update users table; start to setup controller to handle callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,36 @@
 class UsersController < ApplicationController
 
-  def login_form
-    @user = User.new
-  end
+  # def login_form
+  #   @user = User.new
+  # end
 
-  def login
-    username = params[:user][:username]
+  # def login
+  #   username = params[:user][:username]
 
-    found_user = User.find_by(username: username)
+  #   found_user = User.find_by(username: username)
 
-    if found_user
-      # We DID find a user!
-      session[:user_id] = found_user.id
-      flash[:message] = "Logged in as returning user!"
-    else
-      # We did not find an existing user. Let's try to make one!
-      new_user = User.new(username: username)
-      new_user.save
-      # TODO: What happens if saving fails?
-      session[:user_id] = new_user.id
-      flash[:message] = "Created a new user. Welcome!"
-    end
+  #   if found_user
+  #     # We DID find a user!
+  #     session[:user_id] = found_user.id
+  #     flash[:message] = "Logged in as returning user!"
+  #   else
+  #     # We did not find an existing user. Let's try to make one!
+  #     new_user = User.new(username: username)
+  #     new_user.save
+  #     # TODO: What happens if saving fails?
+  #     session[:user_id] = new_user.id
+  #     flash[:message] = "Created a new user. Welcome!"
+  #   end
 
-    return redirect_to root_path
-  end
+  #   return redirect_to root_path
+  # end
 
-  def logout
-    # TODO: What happens if we were never logged in?
-    session[:user_id] = nil
-    flash[:message] = "You have logged out successfully."
-    return redirect_to root_path
-  end
+  # def logout
+  #   # TODO: What happens if we were never logged in?
+  #   session[:user_id] = nil
+  #   flash[:message] = "You have logged out successfully."
+  #   return redirect_to root_path
+  # end
 
   def current
     @user = User.find_by(id: session[:user_id])
@@ -38,6 +38,11 @@ class UsersController < ApplicationController
       head :not_found
       return
     end 
+  end
+
+  def create
+    auth_hash = request.env["omniauth.auth"]
+    raise
   end
 
 end
