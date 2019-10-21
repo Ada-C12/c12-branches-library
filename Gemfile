f136f6ea54792671b3e4b9893b334dd67836c7f4
Jared Maddox <jared@adadevelopersacademy.org>
2019-10-21 12:14:43 -0700

    Setup forwarding to github; callback not yet supported

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.5.5'
 
 gem 'pry-byebug'
 
+gem 'omniauth'
+gem 'omniauth-github'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
@@ -41,6 +44,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'dotenv-rails'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
     resources :books, shallow: true
   end
 
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
-  post "/logout", to: "users#logout", as: "logout"
+  # get "/login", to: "users#login_form", as: "login"
+  # post "/login", to: "users#login"
+  # post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"
+
+  # OAuth support
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "users#create"
 end
