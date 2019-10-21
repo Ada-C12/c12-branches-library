c6ad6b2fe1f67059475329f0afc66fd7d4c487d0
Jared Maddox <jared@adadevelopersacademy.org>
2019-10-21 12:29:01 -0700

    remove username from users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_185826) do
+ActiveRecord::Schema.define(version: 2019_10_21_192808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_10_21_185826) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "uid"
