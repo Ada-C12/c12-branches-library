c6c5c061ec499e6c413b11f32de5cd70f775ba64
Jared Maddox <jared@adadevelopersacademy.org>
2019-10-22 13:33:26 -0700

    user_create sets provider based on param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     auth_hash = request.env["omniauth.auth"]
 
-    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    user = User.find_by(uid: auth_hash[:uid], provider: params[:provider])
     if user
       flash[:success] = "Logged in as returning user #{user.name}"
     else
