06d13edf6192a8d2a238b89d12e6d376648f7640
CheezItMan <chris@adadevelopersacademy.org>
2019-10-23 09:24:19 -0700

    Added login for existing user path

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,24 @@ require "test_helper"
 
 describe UsersController do
 
+  describe "auth_callback" do
+    it "logs in an existing user and redirects them to the root path" do
+      user = users(:jared)
+
+      OmniAuth.config.mock_auth[:github] = 
+            OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      expect {
+        get auth_callback_path(:github)
+      }.wont_change "User.count"
+
+      must_redirect_to root_path
+      expect(session[:user_id]).must_equal user.id
+
+    end
+
+  end
+
   describe "current" do
 
     it "responds with success when user has logged in" do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,6 +9,23 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def setup
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(user)
+    return {
+      provider: user.provider,
+      uid: user.uid,
+      info: {
+        email: user.email,
+        nickname: user.name
+        # You can add more fields from
+        # The omniauth hash here
+      }
+    }
+  end
+
   def perform_login(user = User.first)
     params = {
       user: {
